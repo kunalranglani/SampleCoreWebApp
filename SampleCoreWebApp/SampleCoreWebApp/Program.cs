fafdfba3f88852bb852c7ca548932e44bdee80b1
Kunal Ranglani <kunalranglani@hotmail.com>
2019-07-24 14:02:55 -0700

    Inject AppInsights (#9)
    * Inject AppInsights Nuget Package
    
    * Update Program.cs to use AppInsights
    
    * Update the layout file to use AppInsights
    
    * Add azure-pipelines yaml file

--- SampleCoreWebApp/SampleCoreWebApp/Program.cs
+++ SampleCoreWebApp/SampleCoreWebApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,7 +18,7 @@ namespace SampleCoreWebApp
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost.CreateDefaultBuilder(args).UseApplicationInsights("776e02f8-53f0-4931-8398-8b33480e1478")
                 .UseStartup<Startup>();
     }
 }
